5b6f30e50b4fdb268b822babbb5da91a25ad3e51
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2020-01-05 09:44:40 -0500

    Create leds module

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -3,28 +3,31 @@
 
 // pick a panicking behavior
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+                         // extern crate panic_abort; // requires nightly
+                         // extern crate panic_itm; // logs messages over ITM; requires ITM support
+                         // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
 
+use stm32f3_discovery::delay::Delay;
 use stm32f3_discovery::prelude::*;
 use stm32f3_discovery::stm32;
-use stm32f3_discovery::delay::Delay;
+
+use stm32f3_discovery::leds::{Led, Leds};
 
 #[entry]
 fn main() -> ! {
-
     let device_periphs = stm32::Peripherals::take().unwrap();
     let mut reset_control_clock = device_periphs.RCC.constrain();
     let mut gpioe = device_periphs.GPIOE.split(&mut reset_control_clock.ahb);
-    
-    let mut led = gpioe
-        .pe13
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 
-    led.set_low().unwrap();
+    //TODO: I would prefer to have a method to init the leds all in one go.
+    // let mut leds = stm32f3_discovery::leds::Leds::init(gpioe);
+
+    let mut leds = Leds {
+        ld3: gpioe.pe9.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
+    };
+    leds.ld3.off();
 
     let core_periphs = cortex_m::Peripherals::take().unwrap();
     let mut flash = device_periphs.FLASH.constrain();
@@ -32,7 +35,17 @@ fn main() -> ! {
     let mut delay = Delay::new(core_periphs.SYST, clocks);
 
     loop {
-        led.toggle().unwrap();
+        //use this syntax instead of ld3.toggle() to disambiuate from ToggleableOutputPin
+        //TODO: import just what we need to expose?
+        Led::toggle(&mut leds.ld3);
+        delay.delay_ms(1000u16);
+        Led::toggle(&mut leds.ld3);
+        delay.delay_ms(1000u16);
+
+        //explicit on/off
+        leds.ld3.on();
+        delay.delay_ms(1000u16);
+        leds.ld3.off();
         delay.delay_ms(1000u16);
     }
 }
---
+++ src/leds.rs
@@ -0,0 +1,38 @@
+use stm32f3xx_hal::hal::digital::v2::{OutputPin, ToggleableOutputPin};
+use stm32f3xx_hal::gpio::{Output, PushPull};
+use stm32f3xx_hal::gpio::gpioe::*;
+
+pub trait Led {
+    fn on(&mut self);
+    fn off(&mut self);
+    fn toggle(&mut self);
+}
+
+//TODO: Probably use a macro to generate impls for the right pins
+impl Led for PE9<Output<PushPull>> {
+    fn on(&mut self) {
+        self.set_high().unwrap();
+    }
+    
+    fn off(&mut self) {
+        self.set_low().unwrap();
+    }
+    
+    fn toggle(&mut self) {
+        ToggleableOutputPin::toggle(self).unwrap();
+    }
+}
+
+pub struct Leds {
+    pub ld3: PE9<Output<PushPull>>,
+}
+
+impl Leds {
+    // pub fn init(mut gpioe: Parts) -> Self {
+    //     Leds {
+    //         ld3: gpioe
+    //             .pe9
+    //             .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper),
+    //     }
+    // }
+}
