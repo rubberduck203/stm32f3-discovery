2a21a11abd41c45c86eddc9162c0d9983b5814fa
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2020-01-27 18:08:36 -0500

    flatten match expression

--- examples/button.rs
+++ examples/button.rs
@@ -13,32 +13,30 @@ use stm32f3_discovery::leds::Led;
 #[entry]
 fn main() -> ! {
     let device_periphs = stm32::Peripherals::take().unwrap();
-    let mut reset_control_clock = device_periphs.RCC.constrain();
+    let mut reset_and_clock_control = device_periphs.RCC.constrain();
 
     let core_periphs = cortex_m::Peripherals::take().unwrap();
     let mut flash = device_periphs.FLASH.constrain();
-    let clocks = reset_control_clock.cfgr.freeze(&mut flash.acr);
+    let clocks = reset_and_clock_control.cfgr.freeze(&mut flash.acr);
     let mut delay = Delay::new(core_periphs.SYST, clocks);
 
     // initialize user leds
-    let gpioe = device_periphs.GPIOE.split(&mut reset_control_clock.ahb);
+    let gpioe = device_periphs.GPIOE.split(&mut reset_and_clock_control.ahb);
     let (leds, _gpioe) = stm32f3_discovery::leds::Leds::init(gpioe);
     let mut status_led = leds.ld3;
 
     // initialize user button
-    let gpioa = device_periphs.GPIOA.split(&mut reset_control_clock.ahb);
+    let gpioa = device_periphs.GPIOA.split(&mut reset_and_clock_control.ahb);
     let button = gpioa.pa0; //defaults to Input<Floating>; Has an external pulldown & low pass filter.
+
+    //NOTE: pa0 -> vector exti0
 
     loop {
         delay.delay_ms(50u16);
 
         match button.is_high() {
-            Ok(is_high) => {
-                match is_high {
-                    true => status_led.on(),
-                    false => status_led.off()
-                }
-            },
+            Ok(true) => status_led.on(),
+            Ok(false) => status_led.off(),
             Err(_e) => panic!("Failed to read button state")
         }
     }
