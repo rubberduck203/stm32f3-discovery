0e8ad678aeeb747180809b5aad14d0c40e2a7801
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2020-02-01 13:13:11 -0500

    Extract user_button module

--- examples/button_int.rs
+++ examples/button_int.rs
@@ -3,16 +3,13 @@
 
 extern crate panic_itm;
 
-use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
 
 use stm32f3_discovery::prelude::*;
 use stm32f3_discovery::stm32;
 
 use core::sync::atomic::{AtomicBool, Ordering};
-use stm32::Interrupt;
 use stm32f3_discovery::interrupt;
-
 use stm32f3_discovery::leds::Led;
 
 #[entry]
@@ -25,7 +22,7 @@ fn main() -> ! {
     let (leds, _gpioe) = stm32f3_discovery::leds::Leds::init(gpioe);
     let mut status_led = leds.ld3;
 
-    enable_button_interrupt(&device_periphs.EXTI, &device_periphs.SYSCFG);
+    user_button::interrupt::enable(&device_periphs.EXTI, &device_periphs.SYSCFG);
 
     loop {
         // check to see if flag was active and clear it
@@ -41,34 +38,42 @@ static USER_BUTTON_PRESSED: AtomicBool = AtomicBool::new(false);
 fn EXTI0() {
     // pa0 has a low pass filter on it, so no need to debounce in software
     USER_BUTTON_PRESSED.store(true, Ordering::Relaxed);
-    clear_interrupt_pr0();
+    user_button::interrupt::clear();
 }
 
-fn clear_interrupt_pr0() {
-    const EXTI_PR1: usize = 0x40010414;
-    const PR0: usize = (1 << 0);
-    unsafe{
-        core::ptr::write_volatile(EXTI_PR1 as *mut usize, PR0);
-    }
-}
+mod user_button {
+    pub mod interrupt {
+        use cortex_m::peripheral::NVIC;
+        use stm32::Interrupt;
+        use stm32f3_discovery::stm32;
 
-fn enable_button_interrupt(external_interrupts: &stm32::EXTI, sysconfig: &stm32::SYSCFG) {
-    // enable exti0
-    let interrupt_mask_reg = &external_interrupts.imr1;
-    interrupt_mask_reg.modify(|_, w| w.mr0().set_bit());
+        pub fn clear() {
+            const EXTI_PR1: usize = 0x40010414;
+            const PR0: usize = (1 << 0);
+            unsafe {
+                core::ptr::write_volatile(EXTI_PR1 as *mut usize, PR0);
+            }
+        }
 
-    //TODO: take enum to specify trigger mode {rising, falling, both}
-    // trigger on rising edge
-    let rising_trigger_select_reg = &external_interrupts.rtsr1;
-    rising_trigger_select_reg.modify(|_, w| w.tr0().set_bit());
+        pub fn enable(external_interrupts: &stm32::EXTI, sysconfig: &stm32::SYSCFG) {
+            // enable exti0
+            let interrupt_mask_reg = &external_interrupts.imr1;
+            interrupt_mask_reg.modify(|_, w| w.mr0().set_bit());
 
-    // map line EXTI0 to PA0
-    let external_interrupt_config = &sysconfig.exticr1;
-    const PORT_A_CONFIG: u8 = 0x000;
-    external_interrupt_config.modify(|_, w| unsafe { w.exti0().bits(PORT_A_CONFIG) });
+            //TODO: take enum to specify trigger mode {rising, falling, both}
+            // trigger on rising edge
+            let rising_trigger_select_reg = &external_interrupts.rtsr1;
+            rising_trigger_select_reg.modify(|_, w| w.tr0().set_bit());
 
-    //enable interrupts on EXTI0
-    unsafe {
-        NVIC::unmask(Interrupt::EXTI0);
+            // map line EXTI0 to PA0
+            let external_interrupt_config = &sysconfig.exticr1;
+            const PORT_A_CONFIG: u8 = 0x000;
+            external_interrupt_config.modify(|_, w| unsafe { w.exti0().bits(PORT_A_CONFIG) });
+
+            //enable interrupts on EXTI0
+            unsafe {
+                NVIC::unmask(Interrupt::EXTI0);
+            }
+        }
     }
 }
