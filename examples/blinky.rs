f49041dc0e97fdbc27fe04729536a3f8a524bfd3
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2020-02-02 08:49:06 -0500

    Update dependencies

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,11 +1,7 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_itm;
 
 use cortex_m_rt::entry;
 
