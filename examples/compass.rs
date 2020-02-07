86d90fb8cdef1fc38bce23f187f3a65ce03e12f7
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2020-02-07 18:19:46 -0500

    Use re-exports in compass example

--- examples/compass.rs
+++ examples/compass.rs
@@ -5,13 +5,13 @@ extern crate panic_itm;
 
 use cortex_m_rt::entry;
 
-use stm32f3xx_hal::delay::Delay;
-use stm32f3xx_hal::prelude::*;
-use stm32f3xx_hal::stm32;
+use stm32f3_discovery::stm32f3xx_hal::delay::Delay;
+use stm32f3_discovery::stm32f3xx_hal::prelude::*;
+use stm32f3_discovery::stm32f3xx_hal::stm32;
 
-use switch_hal::{OutputSwitch, ToggleableOutputSwitch};
+use stm32f3_discovery::switch_hal::{OutputSwitch, ToggleableOutputSwitch};
 use stm32f3_discovery::leds::Leds;
-use lsm303dlhc::Lsm303dlhc;
+use stm32f3_discovery::lsm303dlhc::Lsm303dlhc;
 
 #[entry]
 fn main() -> ! {
