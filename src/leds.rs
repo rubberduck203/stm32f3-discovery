e50eb9179186ad62f3032b73c8f416ec11446ff5
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2020-03-21 06:43:39 -0400

    Remove deprecated code

--- src/leds.rs
+++ src/leds.rs
@@ -1,34 +1,8 @@
 //! Provides access to User LEDs LD3-LD10
 use stm32f3xx_hal::gpio::gpioe;
-use stm32f3xx_hal::gpio::{Floating, Input, Output, PushPull};
+use stm32f3xx_hal::gpio::{Output, PushPull};
 
 use switch_hal::{ActiveHigh, IntoSwitch, OutputSwitch, Switch};
-
-/// GpioE after Led pins (PE8-PE15) have been moved
-/// If you intend to use those pins for other functions, DO NOT call Leds::init().
-/// You'll have to initialize the pins yourself.
-#[deprecated(since = "0.3.4", note = "Will be removed in 0.4.0")]
-pub struct GpioE {
-    /// Opaque AFRH register
-    pub afrh: gpioe::AFRH,
-    /// Opaque AFRL register
-    pub afrl: gpioe::AFRL,
-    /// Opaque MODER register
-    pub moder: gpioe::MODER,
-    /// Opaque OTYPER register
-    pub otyper: gpioe::OTYPER,
-    /// Opaque PUPDR register
-    pub pupdr: gpioe::PUPDR,
-
-    pub pe0: gpioe::PE0<Input<Floating>>,
-    pub pe1: gpioe::PE1<Input<Floating>>,
-    pub pe2: gpioe::PE2<Input<Floating>>,
-    pub pe3: gpioe::PE3<Input<Floating>>,
-    pub pe4: gpioe::PE4<Input<Floating>>,
-    pub pe5: gpioe::PE5<Input<Floating>>,
-    pub pe6: gpioe::PE6<Input<Floating>>,
-    pub pe7: gpioe::PE7<Input<Floating>>,
-}
 
 pub struct Leds {
     /// North
@@ -50,89 +24,6 @@ pub struct Leds {
 }
 
 impl Leds {
-    /// Initializes the user LEDs
-    ///
-    /// ## Returns
-    /// A tuple of `(Leds, GpioE)`, where `Leds` has taken ownership of PE8-PE15
-    /// and `GpioE` contains the remaining members of `stm32f3xx_hal::gpio::GPIOE`
-    ///
-    /// **If you intend to use those pins for other functions, DO NOT call Leds::init().**
-    /// You'll have to initialize the pins yourself.
-    #[deprecated(since = "0.3.4", note = "Will be removed in 0.4.0. Use `new` instead.")]
-    pub fn init(mut gpioe: gpioe::Parts) -> (Self, GpioE) {
-        let mut leds = Leds {
-            ld3: gpioe
-                .pe9
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-            ld4: gpioe
-                .pe8
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-            ld5: gpioe
-                .pe10
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-            ld6: gpioe
-                .pe15
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-            ld7: gpioe
-                .pe11
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-            ld8: gpioe
-                .pe14
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-            ld9: gpioe
-                .pe12
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-            ld10: gpioe
-                .pe13
-                .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-                .downgrade()
-                .into_active_high_switch(),
-        };
-
-        //TODO: expose an iterator
-        leds.ld3.off().ok();
-        leds.ld4.off().ok();
-        leds.ld5.off().ok();
-        leds.ld6.off().ok();
-        leds.ld7.off().ok();
-        leds.ld8.off().ok();
-        leds.ld9.off().ok();
-        leds.ld10.off().ok();
-
-        (
-            leds,
-            GpioE {
-                afrh: gpioe.afrh,
-                afrl: gpioe.afrl,
-                moder: gpioe.moder,
-                otyper: gpioe.otyper,
-                pupdr: gpioe.pupdr,
-                pe0: gpioe.pe0,
-                pe1: gpioe.pe1,
-                pe2: gpioe.pe2,
-                pe3: gpioe.pe3,
-                pe4: gpioe.pe4,
-                pe5: gpioe.pe5,
-                pe6: gpioe.pe6,
-                pe7: gpioe.pe7,
-            },
-        )
-    }
-
     /// Initializes the user LEDs to OFF
     pub fn new<PE8Mode, PE9Mode, PE10Mode, PE11Mode, PE12Mode, PE13Mode, PE14Mode, PE15Mode>(
         pe8: gpioe::PE8<PE8Mode>,
